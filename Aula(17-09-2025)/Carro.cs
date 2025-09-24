c8e842fb95ce424b7221806d397d175649ec6827
Diegothom4z <thomaz.diego@estudante.ifro.edu.br>
2025-09-24 19:54:58 -0400

    Inicio do projeto

---
+++ Aula(17-09-2025)/Carro.cs
@@ -0,0 +1,51 @@
+﻿
+
+public class Carro
+{
+    //parametros
+    public int _id;
+    public string _marca;
+    public string _modelo;
+    public string _placa;
+    public string _renavam;
+    public string _chassi;
+
+    //metodo construtor
+    public Carro (string chassi)
+    {
+        if (ValidarChassi(chassi))
+        {
+            _chassi = chassi;
+        }
+
+    }
+
+    public Carro(int id, string marca, string modelo, string placa, string renavam, string chassi)
+    {
+        if (ValidarChassi(chassi))
+        {
+            _chassi = chassi;
+        }
+
+        _id = id;
+        _marca = marca;
+        _modelo = modelo;
+        _placa = placa;
+        _renavam = renavam;
+        _chassi = chassi;
+    }
+
+    public bool ValidarChassi(string chassi)
+    {
+        chassi = chassi.ToUpper();
+        if (chassi.Length == 17 && !chassi.Contains("i") && !chassi.Contains("o") && !chassi.Contains("q"))
+        {
+           return true;
+
+        }
+        throw new ArgumentException("CHASSI INVALIDO!");
+
+
+
+    }
+}
---
+++ Aula(17-09-2025)/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+try
+{
+    Carro carro1 = new Carro("10203040906070090");
+    Console.WriteLine($"O chassi informado foi: {carro1._chassi}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+//espera uma tecla ser precionada para incerrar
+Console.ReadKey();
+
+////trata do metodo contrutor pedindo todos os parametros
+//Carro carro2 = new Carro(1, "VW", "Gol", "102030", "20304050", "12345678910111213");
